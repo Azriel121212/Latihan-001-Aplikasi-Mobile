80afdeeec35d423233904b93908c0809bc84375d
Azriel121212 <tehpociiki@gmail.com>
2026-09-22 22:10:24 +0700

    latihan1.dart

---
+++ latihan1.dart
@@ -0,0 +1,31 @@
+void main() {
+  print("cihuy");
+  print("cihuy");
+  
+  String produk ="Parfum wakacaw";
+  int stok=10;
+  double harga=250.000;
+  bool tersedia=true;
+  
+  print(produk);
+  print(stok);
+  print(harga);
+  print(tersedia);
+  
+  String alamat = "kebon cau";
+  print(alamat);
+  
+  String? itemName = 'Kopi Hitam';
+  print(itemName);
+  itemName = null;
+  print(itemName);
+  
+  String dailyinmylife ="Rafly & Cahyo";
+  print(dailyinmylife);
+  
+  String? customerNote;
+  customerNote = 'Kurang manis';
+  customerNote = null;
+  print(customerNote);
+  
+}

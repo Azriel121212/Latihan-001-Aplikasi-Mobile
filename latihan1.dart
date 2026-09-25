9c809a5481deab34cbfc1e33deffb0728bd3d8b9
Chaerul Azriel Ardiansyah <tehpociiki@gmail.com>
2026-09-25 18:14:43 +0700

    Update latihan1.dart

--- latihan1.dart
+++ latihan1.dart
@@ -2,48 +2,50 @@ void main() {
   print("cihuy");
   print("cihuy");
   
-  String produk = "Parfum wakacaw";
-  int stok = 10;
-  int harga = 275500; 
-  double ratingParfum = 4.8; 
-  double beratKg = 0.25; 
+  String produk = "Mie jebew level 7";
+  int stok = 21;
+  int harga = 15000; 
+  double ratingmakanan = 4.9; 
+  double beratporsi = 0.3; 
   bool tersedia = true;
   
   print(produk);
   print(stok);
   print(harga);
-  print(ratingParfum);
-  print(beratKg);
+  print(ratingmakanan);
+  print(beratporsi);
   print(tersedia);
   
   String alamat = "kebon cau";
   print(alamat);
   
-  print('Promo! $produk sisa $stok pcs. Dikirim ke $alamat.');
+  print('$produk sisa $stok porsi dikirim ke $alamat');
   
-  final String kurir = 'SiCepat';
-  const int ongkir = 12000;
-  print('Dikirim pakai $kurir, ongkir Rp$ongkir');
+  final String kurir = 'GoFood';
+  const int ongkir = 10000;
+  print('dikirim pakai $kurir ongkir Rp$ongkir');
   
-  String? itemName = 'Kopi Hitam';
-  print(itemName);
-  itemName = null;
-  print(itemName);
+  String? topping = 'Topping Keju';
+  print(topping);
+  topping = null;
+  print(topping);
   
   String dailyinmylife = "Rafly & Cahyo";
   print(dailyinmylife);
   
-  String? customerNote;
-  customerNote = 'Kurang manis';
-  customerNote = null;
-  print(customerNote);
-  
-  List<String> menuwarteg = [
-    'Ayam Bakar',
-    'Pepes Ikan',
-    'Sayur Asem',
-    'Pecel Sayur',
-    'Ikan Tongkol',
+  List<String> menufavorit = [
+    'Mie jebew',
+    'Seblak tulang',
+    'Basreng guntur doa ibu',
+    'Es teh solo anget',
+    'Dimsum udang',
   ];
-  print(menuwarteg);
+  print(menufavorit);
+  print(menufavorit[0]);
+  print(menufavorit[1]);
+  print(menufavorit[2]);
+  print(menufavorit[3]);
+  print(menufavorit[4]);
+  
+  print('Rafly & Cahyo doyan makan '+ menufavorit[2]);
 }
